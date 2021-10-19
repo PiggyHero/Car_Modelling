15696b838648b8eb6c4f75136d15dee903a953e4
pigpighero <363158526@qq.com>
2021-10-19 20:29:49 +0800

    小车建模

---
+++ build/car_model/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pigpighero/DEMO01_WS/src/car_model
+# Build directory: /home/pigpighero/DEMO01_WS/build/car_model
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/pigpighero/DEMO01_WS/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/pigpighero/DEMO01_WS/devel;/home/pigpighero/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/pigpighero/DEMO01_WS/devel;/home/pigpighero/catkin_ws/devel;/opt/ros/melodic' != '' else []
